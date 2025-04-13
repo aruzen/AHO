ba53ff604bc1c54e03eadd5a554138453e038b1b
aruzen <aruzenueii@gmail.com>
2025-04-13 20:58:27 +0900

    fix: rename Polygon::points -> Polygon::_points

--- AHOLi/AHO/core/Polygon.h
+++ AHOLi/AHO/core/Polygon.h
@@ -13,13 +13,13 @@
 
 namespace AHO_NAMESPACE {
 	// TODO
-	template<typename R = int,
+	template<typename R,
 			 VSL_NAMESPACE::is_dimention D,
 			 typename CoordinateInfo,
 			 typename Size,
 			 typename Alloc>
 	struct Polygon : public std::vector<Alloc, IndexedTriangle<Size>> {
-		std::map<AHO_NAMESPACE::_Point<R, D, CoordinateInfo>, Size> points;
+		std::map<AHO_NAMESPACE::_Point<R, D, CoordinateInfo>, Size> _points;
 
 		constexpr Polygon() {}; 
 		constexpr Polygon(std::initializer_list<AHO_NAMESPACE::Triangle<R, D, CoordinateInfo>> init) {};
@@ -36,15 +36,19 @@ namespace AHO_NAMESPACE {
 	template<AHO_NAMESPACE::concepts::points_view_able T>
 	inline constexpr Polygon<R, D, CI, Size, Alloc> AHO_NAMESPACE::Polygon<R, D, CI, Size, Alloc>::operator+(T t)
 	{
-		Polygon<R, D, CI, Size, Alloc> copy(*this);
-
-		return copy;
+		return Polygon<R, D, CI, Size, Alloc>(*this);
 	}
 
 	template<typename R, VSL_NAMESPACE::is_dimention D, typename CI, typename Size, typename Alloc>
 	inline constexpr double AHO_NAMESPACE::Polygon<R, D, CI, Size, Alloc>::area()
 	{
-		return 0.0;
+        if constexpr (std::same_as<D, VSL_NAMESPACE::D1>) {
+            return 0.0;
+        } else if constexpr (std::same_as<D, VSL_NAMESPACE::D2>) {
+            return 0.0;
+        } else if constexpr (std::same_as<D, VSL_NAMESPACE::D3>) {
+            return 0.0;
+        }
 	}
 
 	template<typename R, VSL_NAMESPACE::is_dimention D, typename CI, typename Size, typename Alloc>

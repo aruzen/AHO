236a75e173275c819aea69072e208da980d0e739
aruzen <aruzenueii@gmail.com>
2025-06-11 12:01:56 +0900

    update: ShaderType

--- VSLi/VSL/Vulkan/shader.cpp
+++ VSLi/VSL/Vulkan/shader.cpp
@@ -9,12 +9,13 @@
 #include "shader.h"
 
 #include <fstream>
+#include <memory>
 #include <source_location>
 
 template<VSL_NAMESPACE::ShaderType _Type>
 vsl::Shader<_Type>::Shader(vsl::LogicalDeviceAccessor device, std::filesystem::path path)
 {
-	_data = std::shared_ptr<_impl::Shader_impl>(new _impl::Shader_impl);
+	_data = std::make_shared<_impl::Shader_impl>();
 	_data->device = device._data;
 	std::vector<char> code;
 
@@ -40,7 +41,7 @@ vsl::Shader<_Type>::Shader(vsl::LogicalDeviceAccessor device, std::filesystem::p
 
 	_data->name = path.filename().string();
 
-	_data->shaderStageCreateInfo.pName = "main";
+	_data->shaderStageCreateInfo.pName = _data->name.c_str();
 	_data->shaderStageCreateInfo.module = _data->shaderModule;
 	if constexpr (_Type == ShaderType::Vertex) {
 		_data->shaderStageCreateInfo.stage = vk::ShaderStageFlagBits::eVertex;
@@ -52,7 +53,7 @@ vsl::Shader<_Type>::Shader(vsl::LogicalDeviceAccessor device, std::filesystem::p
 template<VSL_NAMESPACE::ShaderType _Type>
 vsl::Shader<_Type>::Shader(vsl::LogicalDeviceAccessor device, std::filesystem::path path, std::string name)
 {
-	_data = std::shared_ptr<_impl::Shader_impl>(new _impl::Shader_impl);
+	_data = std::make_shared<_impl::Shader_impl>();
 	_data->device = device._data;
 	std::vector<char> code;
 
@@ -78,14 +79,9 @@ vsl::Shader<_Type>::Shader(vsl::LogicalDeviceAccessor device, std::filesystem::p
 
 	_data->name = name;
 
-	_data->shaderStageCreateInfo.pName = "main";
+	_data->shaderStageCreateInfo.pName = _data->name.c_str();
 	_data->shaderStageCreateInfo.module = _data->shaderModule;
-	if constexpr (_Type == ShaderType::Vertex) {
-		_data->shaderStageCreateInfo.stage = vk::ShaderStageFlagBits::eVertex;
-	}
-	else if constexpr (_Type == ShaderType::Fragment) {
-		_data->shaderStageCreateInfo.stage = vk::ShaderStageFlagBits::eFragment;
-	}
+    _data->shaderStageCreateInfo.stage = (vk::ShaderStageFlagBits)_Type;
 }
 
 VSL_NAMESPACE::_impl::Shader_impl::~Shader_impl()
@@ -93,5 +89,22 @@ VSL_NAMESPACE::_impl::Shader_impl::~Shader_impl()
 	device->device.destroyShaderModule(shaderModule);
 }
 
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Error>;
 template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Vertex>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::TessellationControl>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::TessellationEvaluation>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Geometry>;
 template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Fragment>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Compute>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::AllGraphics>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Raygen>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::AnyHit>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::ClosestHit>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Miss>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Intersection>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Callable>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Task>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::Mesh>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::SubpassShadingHuawei>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::ClusterCullingHuawei>;
+template struct vsl::Shader<VSL_NAMESPACE::ShaderType::All>;
--- VSLi/VSL/Vulkan/shader.h
+++ VSLi/VSL/Vulkan/shader.h
@@ -9,10 +9,26 @@
 #include "device.h"
 
 namespace vsl {
-	enum class ShaderType : unsigned char {
-		Error,
-		Vertex,
-		Fragment
+	enum class ShaderType : int {
+        Error,
+        Vertex = 0x00000001,
+        TessellationControl = 0x00000002,
+        TessellationEvaluation = 0x00000004,
+        Geometry = 0x00000008,
+        Fragment = 0x00000010,
+        Compute = 0x00000020,
+        AllGraphics = 0x0000001F,
+        Raygen = 0x00000100,
+        AnyHit = 0x00000200,
+        ClosestHit = 0x00000400,
+        Miss = 0x00000800,
+        Intersection = 0x00001000,
+        Callable = 0x00002000,
+        Task = 0x00000040,
+        Mesh = 0x00000080,
+        SubpassShadingHuawei = 0x00004000,
+        ClusterCullingHuawei = 0x00080000,
+        All = 0x7FFFFFFF,
 	};
 
 	struct ShaderAccessor {
